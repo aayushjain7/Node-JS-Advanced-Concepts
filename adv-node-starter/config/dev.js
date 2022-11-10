3c51dd4e4abab9c67219bba3fbfab870eb71bad8
Aayush <jaayush014@gmail.com>
2022-11-10 17:06:44 +0000

    setup advance note concept project

--- adv-node-starter/config/dev.js
+++ adv-node-starter/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
 	googleClientID:
-		"38830527740-noe0lsbbd62ctd28o6e2fh0p5khpbkes.apps.googleusercontent.com",
+		"70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
 	googleClientSecret: "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",
 	mongoURI:
 		"mongodb://aayushjain:aayushjain@ac-uh80hxr-shard-00-00.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-01.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-02.qqndy83.mongodb.net:27017/AdvancedNodeConceptUdemy?ssl=true&replicaSet=atlas-8olp8w-shard-0&authSource=admin&retryWrites=true&w=majority",
