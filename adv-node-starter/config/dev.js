382db0883ceed9b05fad7febeb6138f5567e4330
Aayush <jaayush014@gmail.com>
2022-11-11 17:55:46 +0000

    cleanup

--- adv-node-starter/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	googleClientID:
-		"70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-	googleClientSecret: "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",
-	mongoURI:
-		"mongodb://aayushjain:aayushjain@ac-uh80hxr-shard-00-00.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-01.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-02.qqndy83.mongodb.net:27017/AdvancedNodeConceptUdemy?ssl=true&replicaSet=atlas-8olp8w-shard-0&authSource=admin&retryWrites=true&w=majority",
-	cookieKey: "123123123",
-};
