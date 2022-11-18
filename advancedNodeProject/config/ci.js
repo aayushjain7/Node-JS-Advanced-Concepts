d7c91fef5b76c49c1a121644de1752f0149f468e
aayush-toddleapp <aayush@toddleapp.com>
2022-11-18 23:53:26 +0530

    continuous integration with travis-ci

---
+++ advancedNodeProject/config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+	googleClientID:
+		'38830527740-noe0lsbbd62ctd28o6e2fh0p5khpbkes.apps.googleusercontent.com',
+	googleClientSecret: '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',
+	mongoURI: 'mongodb://127.0.0.1:27017/AdvancedNodeConceptUdemy_ci',
+	cookieKey: '123123123',
+	redisUrl: 'redis://127.0.0.1:6379',
+};
--- advancedNodeProject/config/dev.js
+++ advancedNodeProject/config/dev.js
@@ -1,8 +1,9 @@
 module.exports = {
 	googleClientID:
-		"38830527740-noe0lsbbd62ctd28o6e2fh0p5khpbkes.apps.googleusercontent.com",
-	googleClientSecret: "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",
+		'38830527740-noe0lsbbd62ctd28o6e2fh0p5khpbkes.apps.googleusercontent.com',
+	googleClientSecret: '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',
 	mongoURI:
-		"mongodb://aayushjain:aayushjain@ac-uh80hxr-shard-00-00.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-01.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-02.qqndy83.mongodb.net:27017/AdvancedNodeConceptUdemy?ssl=true&replicaSet=atlas-8olp8w-shard-0&authSource=admin&retryWrites=true&w=majority",
-	cookieKey: "123123123",
+		'mongodb://aayushjain:aayushjain@ac-uh80hxr-shard-00-00.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-01.qqndy83.mongodb.net:27017,ac-uh80hxr-shard-00-02.qqndy83.mongodb.net:27017/AdvancedNodeConceptUdemy?ssl=true&replicaSet=atlas-8olp8w-shard-0&authSource=admin&retryWrites=true&w=majority',
+	cookieKey: '123123123',
+	redisUrl: 'redis://127.0.0.1:6379',
 };
